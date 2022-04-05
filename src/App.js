62d838d71df61b5e1e911f153e1a495e2070e018
Anjor2003 <anjor2003@hotmail.com>
2022-04-05 12:57:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import '../node_modules/swiper/swiper-bundle.min.css'
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import './App.scss'
+
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+
+import Routers from './config/Routes'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Route
+        render={(props) => (
+          <>
+            <Header {...props} />
+            <Routers />
+            <Footer />
+          </>
+        )}
+      />
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { OutLineButton } from '../components/button/Button'
+
+import HeroSlide from '../components/hero-slide/HeroSlide'
+import Movielist from '../components/movie-list/MovieList'
+
+import { category, movieType, tvType } from '../api/tmdbApi'
+
+const Home = () => {
+  return (
+    <>
+      <HeroSlide />
+      <div className='container'>
+        <div className='secion mb-3'>
+          <div className='section__header mb-2'>
+            <h2>Trending Movies</h2>
+            <Link to='/movie'>
+              <OutLineButton className='small'>View More</OutLineButton>
+            </Link>
+          </div>
+          <Movielist category={category.movie} type={movieType.popular} />
+        </div>
+        <div className='secion mb-3'>
+          <div className='section__header mb-2'>
+            <h2>Top Rated</h2>
+            <Link to='/movie'>
+              <OutLineButton className='small'>View More</OutLineButton>
+            </Link>
+          </div>
+          <Movielist category={category.movie} type={movieType.top_rated} />
+        </div>
+        <div className='secion mb-3'>
+          <div className='section__header mb-2'>
+            <h2>Trending TV</h2>
+            <Link to='/tv'>
+              <OutLineButton className='small'>View More</OutLineButton>
+            </Link>
+          </div>
+          <Movielist category={category.tv} type={tvType.popular} />
+        </div>
+        <div className='secion mb-3'>
+          <div className='section__header mb-2'>
+            <h2>Top Rated TV </h2>
+            <Link to='/tv'>
+              <OutLineButton className='small'>View More</OutLineButton>
+            </Link>
+          </div>
+          <Movielist category={category.tv} type={tvType.top_rated} />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
